81a3ab36fcc89921b8d37801107c2b2b25b88d6d
Amelia Tay <amelia@open.gov.sg>
2023-10-22 16:33:54 +0800

    Add china flag

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def img_to_bytes(img_path):
 
 def cs_sidebar():
 
-    st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
+    st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("assets/china-flag.png")), unsafe_allow_html=True)
     st.sidebar.header('Firm characteristics of china-listed companies')
 
     st.sidebar.markdown('''
